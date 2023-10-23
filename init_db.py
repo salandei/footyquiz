32660a0887e4b129a5c934916c1c1ea2e2e6b267
Solomon Alan-Dei <solomon.alan-dei@kibo.school>
2023-10-23 22:07:10 +0200

    update init file for database to include adding questions

--- init_db.py
+++ init_db.py
@@ -5,7 +5,27 @@ connection = sqlite3.connect('database.db')
 print("Initializing db...")
 with open('db_schema.sql') as f:
     connection.executescript(f.read())
+    
+connection.commit()
+print("DONE!")
+
+import json
+
+print("Inserting questions...")
+
+query = "INSERT INTO q_and_a (question, answers, correct_answer) VALUES (:question, :answers, :correct_answer)"
+questions = dict()
+with open("questions.json", "r") as question_file:
+    questions = json.load(question_file)
+for data in questions.values():
+    args = {
+        "question": data["question"],
+        "answers": data["answers"],
+        "correct_answer": data["correct_answer"]
+    }
+    connection.execute(query, args)
 
 connection.commit()
 connection.close()
-print("DONE!")
+
+print("Inserting questions...DONE!")
