e5dd7d68fb489936e0d26363af8a66540f2d110f
Solomon Alan-Dei <solomon.alan-dei@kibo.school>
2023-10-23 20:16:32 +0200

    Update initialization code

--- init_db.py
+++ init_db.py
@@ -2,10 +2,10 @@ import sqlite3
 
 connection = sqlite3.connect('database.db')
 
-print("initializing db...")
+print("Initializing db...")
 with open('db_schema.sql') as f:
     connection.executescript(f.read())
 
 connection.commit()
 connection.close()
-print("finished!")
+print("DONE!")
