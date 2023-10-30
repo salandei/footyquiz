1c959d173d23214a2e21e438d76c7ea1f0282f1f
Solomon Alan-Dei <solomon.alan-dei@kibo.school>
2023-10-30 17:56:59 +0100

    update app initialization code

--- init_db.py
+++ init_db.py
@@ -1,31 +1,46 @@
 import sqlite3
-
-connection = sqlite3.connect('database.db')
-
-print("Initializing db...")
-with open('db_schema.sql') as f:
-    connection.executescript(f.read())
-    
-connection.commit()
-print("DONE!")
-
 import json
+import os.path as filepath
 
-print("Inserting questions...")
+DATABASE = 'database.db'
+QUESTIONS = 'questions.json'
+    
+###############################
+# POPULATE DB QUESTIONS TABLE
+###############################
+def add_questions_db(json_file):    
+    print("Inserting questions...")
 
-query = "INSERT INTO q_and_a (question, answers, correct_answer) VALUES (:question, :answers, :correct_answer)"
-questions = dict()
-with open("questions.json", "r") as question_file:
-    questions = json.load(question_file)
-for data in questions.values():
-    args = {
-        "question": data["question"],
-        "answers": data["answers"],
-        "correct_answer": data["correct_answer"]
-    }
-    connection.execute(query, args)
+    query = "INSERT INTO q_and_a (question, answers, correct_answer) VALUES (:question, :answers, :correct_answer)"
+    questions = dict()
+    with open(json_file, "r") as question_file:
+        questions = json.load(question_file)
 
-connection.commit()
-connection.close()
+    for data in questions.values():
+        args = {
+            "question": data["question"],
+            "answers": data["answers"],
+            "correct_answer": data["correct_answer"]
+        }
+        connection.execute(query, args)
+    connection.commit()
 
-print("Inserting questions...DONE!")
+    print("Inserting questions...DONE!")
+
+#############################
+# INITIALIZE THE DATABASE
+#############################
+def initialize_db():
+    print("Initializing db...")
+    with open('db_schema.sql') as f:
+        connection.executescript(f.read())       
+    connection.commit()
+
+if __name__ == "__main__":
+    # Create database file and connect to it for initialization
+    if(not filepath.exists(DATABASE)):
+        if(open(DATABASE, "x")):
+            connection = sqlite3.connect('database.db')
+            initialize_db()
+            add_questions_db(QUESTIONS)
+            connection.close()
